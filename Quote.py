a52e7eef1281518092104d9c277b1c04f891e376
clarakja <33611179+clarakja@users.noreply.github.com>
2025-01-07 23:16:52 +0900

    Update Quote.py

--- Quote.py
+++ Quote.py
@@ -6,6 +6,7 @@ quotes = [
     "The only limit to our realization of tomorrow is our doubts of today.",
     "Do what you can, with what you have, where you are.",
     "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    "상황을 가장 잘 활용하는 사람이 가장 좋은 상황을 맞는다.",
     "The best time to plant a tree was 20 years ago. The second best time is now.",
     "It does not matter how slowly you go as long as you do not stop.",
     "큰 목표를 이루고 싶으면 허락을 구하지 마라. "
